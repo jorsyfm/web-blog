db37612b77c92fda8a29692815d4b773d46fbf77
Jorsy <jorsy.fm@gmail.com>
2019-04-02 17:55:06 -0600

    Form Register

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,15 +4,16 @@ import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
+// Import for Form
+import { FormsModule } from '@angular/forms';
+
 // Import Font Awesome
-import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
-import { library } from '@fortawesome/fontawesome-svg-core';
-import { fas } from '@fortawesome/free-solid-svg-icons';
+import { AngularFontAwesomeModule } from 'angular-font-awesome';
+
 import { LoginComponent } from './components/login/login.component';
 import { RegisterComponent } from './components/register/register.component';
 import { HomeComponent } from './components/home/home.component';
 import { ErrorComponent } from './components/error/error.component';
-library.add(fas);
 
 @NgModule({
   declarations: [
@@ -25,7 +26,8 @@ library.add(fas);
   imports: [
     BrowserModule,
     AppRoutingModule,
-    FontAwesomeModule
+    FormsModule,
+    AngularFontAwesomeModule
   ],
   providers: [],
   bootstrap: [AppComponent]
