583b82713ee141818f0cf8e0d434d0967351310d
Jorge Alberto Fernández Morales <jorsy.fm@gmail.com>
2019-04-01 22:29:58 -0600

    Componentes
    Creación de componentes básicos y de sistema de rutas.

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,25 @@
+import { ErrorComponent } from './components/error/error.component';
+// Importar componentes
+import { HomeComponent } from './components/home/home.component';
+import { RegisterComponent } from './components/register/register.component';
+import { LoginComponent } from './components/login/login.component';
+
+// Imports para rutas
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+// Definir rutas
+const routes: Routes = [
+  { path: 'home', component: HomeComponent} ,
+  { path: 'login', component: LoginComponent },
+  { path: 'registro', component: RegisterComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' },
+  { path: '**', component: ErrorComponent }
+];
+
+// Exportar configuración
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { AppRoutingModule } from './app-routing.module';
 
 import { AppComponent } from './app.component';
 
@@ -7,14 +8,23 @@ import { AppComponent } from './app.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 import { library } from '@fortawesome/fontawesome-svg-core';
 import { fas } from '@fortawesome/free-solid-svg-icons';
+import { LoginComponent } from './components/login/login.component';
+import { RegisterComponent } from './components/register/register.component';
+import { HomeComponent } from './components/home/home.component';
+import { ErrorComponent } from './components/error/error.component';
 library.add(fas);
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    HomeComponent,
+    ErrorComponent
   ],
   imports: [
     BrowserModule,
+    AppRoutingModule,
     FontAwesomeModule
   ],
   providers: [],
