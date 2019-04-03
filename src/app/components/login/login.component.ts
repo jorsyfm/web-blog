180a0a1a19441733ac6f105921ab54a4a7bf8871
Jorge Alberto Fernández Morales <jorsy.fm@gmail.com>
2019-04-03 00:11:39 -0600

    Servicio Login
    Iniciando formulario y servicio para Login

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,19 +1,30 @@
+import { UserService } from './../../services/user.service';
+import { User } from './../../models/user';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.css']
+  styleUrls: ['./login.component.css'],
+  providers: [UserService]
 })
 export class LoginComponent implements OnInit {
 
-  public pageTitle: string;
+  public user: User;
 
-  constructor( ) {
-    this.pageTitle = 'Login';
+  constructor( private _userService: UserService ) {
+    this.user = new User(1, '', '', 'ROLE_USER', '', '', '', '');
   }
 
   ngOnInit() {
+  }
+
+  onSubmit(form: any) {
+    this._userService.login(this.user).subscribe( response => {
+      console.log(response);
+    }, error => {
+      console.log( <any>error );
+    });
   }
 
 }
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -29,4 +29,23 @@ export class UserService {
     // Petición AJAX
     return this._http.post(this.url+'register', params, {headers: headers});
   }
+
+  login(user, gettoken = null): Observable<any> {
+
+    // Saber si quiero obtener la información del usuario o el Token
+    if (gettoken != null) {
+      user.gettoken = 'true';
+    }
+    // Convertir parámetro en JSON string
+    let json = JSON.stringify(user);
+
+    // Definir parámetros a enviar
+    let params = 'json=' + json;
+
+    // Cabezeras
+    let headers = new HttpHeaders().set('Content-Type', 'application/x-www-form-urlencoded');
+
+    // Petición AJAX
+    return this._http.post(this.url + 'login', params, {headers: headers});
+  }
 }
